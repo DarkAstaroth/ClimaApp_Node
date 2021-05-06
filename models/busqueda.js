873719a65241779b9e608c76718681616d314139
Manuel <mninahuanca4@gmail.com>
2021-05-05 23:52:06 -0400

    crear instancias de axios

--- models/busqueda.js
+++ models/busqueda.js
@@ -7,18 +7,31 @@ class Busquedas {
 
     }
 
+    get paramsMapbox() {
+        return {
+            'access_token': 'pk.eyJ1IjoiZGFya2FzdGFyb3RoIiwiYSI6ImNrb2NidnVuMjA1czYydW4xcmZqNTY1dnkifQ.TqrBba76H4a2icWzbW0QUw',
+            'limit': 5,
+            'language':'es'
+        }
+    }
+
     async ciudad(lugar = '') {
 
         try {
 
             // peticion http
-            // console.log('ciudad', lugar);
-            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/la%20paz.json?access_token=pk.eyJ1IjoiZGFya2FzdGFyb3RoIiwiYSI6ImNrb2NidnVuMjA1czYydW4xcmZqNTY1dnkifQ.TqrBba76H4a2icWzbW0QUw&autocomplete=true&limit=5&language=es');
+            
+            const intance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox
+            });
+
+            const resp = await intance.get();     
             console.log(resp.data);
             return [];
 
         } catch (error) {
-
+            console.log(error);
             return [];
 
         }
