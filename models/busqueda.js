8f17c011f937aca8271d50fb33dc6b0935748312
Manuel <mninahuanca4@gmail.com>
2021-05-05 23:37:14 -0400

    Mapbox search API y token de acceso

--- models/busqueda.js
+++ models/busqueda.js
@@ -13,7 +13,7 @@ class Busquedas {
 
             // peticion http
             // console.log('ciudad', lugar);
-            const resp = await axios.get('https://reqres.in/api/users?page=2');
+            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/la%20paz.json?access_token=pk.eyJ1IjoiZGFya2FzdGFyb3RoIiwiYSI6ImNrb2NidnVuMjA1czYydW4xcmZqNTY1dnkifQ.TqrBba76H4a2icWzbW0QUw&autocomplete=true&limit=5&language=es');
             console.log(resp.data);
             return [];
 
