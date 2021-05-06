b2c22c99261921dc6364831b980162a22ea7acb2
Manuel <mninahuanca4@gmail.com>
2021-05-05 23:23:10 -0400

    Realizar peticiones Http desde node

--- models/busqueda.js
+++ models/busqueda.js
@@ -1,15 +1,27 @@
+const axios = require('axios');
+
 
 class Busquedas {
     historial = ['La Paz', 'Cochabamba', 'Santa Cruz'];
     constructor() {
-        
+
     }
 
     async ciudad(lugar = '') {
-        // peticion http
-        console.log(lugar);
 
-        return [];// retornar los lugares 
+        try {
+
+            // peticion http
+            // console.log('ciudad', lugar);
+            const resp = await axios.get('https://reqres.in/api/users?page=2');
+            console.log(resp.data);
+            return [];
+
+        } catch (error) {
+
+            return [];
+
+        }
     }
 }
 
